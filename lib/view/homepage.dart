c534247a75b862e63144d576d054f51d682ce1a3
Thamizhselvan03 <thamizhselvan273@gmail.com>
2022-11-25 17:34:36 +0530

    snack bar

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,51 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shortcut/snackBar/snackbar.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-final ButtonStyle flatButtonStyle = TextButton.styleFrom(
-  foregroundColor: Colors.black87,
-  minimumSize: const Size(88, 36),
-  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-  shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2.0)),
-  ),
-);
-
-final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
-  foregroundColor: Colors.black87,
-  backgroundColor: Colors.grey[300],
-  minimumSize: const Size(88, 36),
-  padding: const EdgeInsets.symmetric(horizontal: 16),
-  shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
-  ),
-);
-final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
-  foregroundColor: Colors.black87,
-  minimumSize: const Size(88, 36),
-  padding: const EdgeInsets.symmetric(horizontal: 16),
-  shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
-  ),
-).copyWith(
-  side: MaterialStateProperty.resolveWith<BorderSide>(
-    (Set<MaterialState> states) {
-      if (states.contains(MaterialState.pressed)) {
-        return const BorderSide(
-          color: Colors.green,
-          //color: Theme.of(context).colorScheme.primary,
-          width: 1,
-        );
-      }
-      return const BorderSide(
-        color: Colors.green,
-        //color: Theme.of(context).colorScheme.primary,
-        width: 1,
-      );
-      // Defer to the widget's default.
-    },
-  ),
-);
+// Defer to the widget's default.
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -54,26 +12,63 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SizedBox(
-          width: MediaQuery.of(context).size.width * 1,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          backgroundColor: Vx.white,
+          body:VStack([
+            "djfj djf ksd disjf".text.make().box.green400.make(),
+               "djfj djf ksd disjf".text.make().box.red200.make(),
+                "djfj djf ksd disjf".text.make().box.pink50.make(),
+            HStack([
+              "djfj djf ksd dsfd  disjf".text.make().box.green400.make(),
+              "djfj djf ksd sdfds sefsd es disjf".text.make().box.red200.make(),
+              "djfj djf ksd dsfdsfdsf disjf".text.make().box.pink50.make(),
+            ]).scrollHorizontal(),
+            ZStack([
+              "djfj djf ksd disjf".text.make().box.green400.make(),
+              "djfj djf ksd disjf".text.make().box.red200.make(),
+              "djfj djf ksd disjf".text.make().box.pink50.make(),
 
-                  TextButton(onPressed: (){
-                   CustomSnackBar.showSuccessSnackbar(context: context, titte: "hai", message: "rai");
-
-                  }, child:const Text(
-                    "snack Bar"
-
-                  ))
             ],
-          ),
-        ),
-      ),
+              alignment: AlignmentDirectional.center,
+
+            ),
+           [
+              "djfj djf ksd disjf".text.make().box.green400.make(),
+              "djfj djf ksd disjf".text.make().box.red200.make(),
+              "djfj djf ksd disjf".text.make().box.pink50.make(),
+            ].hStack().scrollHorizontal(),
+            [
+              "djfj djf ksd disjf".text.make().box.green400.make(),
+              "djfj djf ksd disjf".text.make().box.red200.make(),
+              "djfj djf ksd disjf".text.make().box.pink50.make(),
+            ].vStack(),
+            [
+              "djfj djf ksd disjf".text.make().box.green400.make(),
+              "djfj djf ksd disjf".text.make().box.red200.make(),
+              "djfj djf ksd disjf".text.make().box.pink50.make(),
+            ].zStack(),
+            const VxDiscList(["hdsf","item 1","item 2","item 3"],color: Vx.green900,),
+            ['df dhfj',"dhf","sjdhfjd"].decimalList().color(Colors.brown).p12(),
+
+          //  Instead of Flex
+            VxBlock(
+              children: [
+                "djfj djf ksd disjf".text.make().box.green400.make(),
+                "djfj djf ksd disjf".text.make().box.red200.make(),
+                "djfj djf ksd disjf".text.make().box.pink50.make(),
+                VxInlineBlock(
+                  children: [
+                    "djfj djf ksd disjf".text.make().box.green400.make(),
+                    "djfj djf ksd disjf".text.make().box.red200.make(),
+                    "djfj djf ksd disjf".text.make().box.pink50.make(),
+                  ],
+                ).p12()
+              ],
+            ).p12()
+
+          ]),
+
+
+      )
     );
   }
 }
-
-
